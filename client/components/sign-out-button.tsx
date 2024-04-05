8e172925d754ef5368a355758440862c532d7e91
조성원 <tjddnjsjo@kakao.com>
2024-04-05 14:58:54 +0900

    feat: 로그아웃 확인을 위한 drawer 적용

--- client/components/sign-out-button.tsx
+++ client/components/sign-out-button.tsx
@@ -5,6 +5,16 @@ import Cookies from 'js-cookie'
 
 import { signOut } from '@/api/auth'
 import { Button } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 
 function SignOutButton() {
   const router = useRouter()
@@ -20,9 +30,25 @@ function SignOutButton() {
   }
 
   return (
-    <Button variant="ghost" onClick={handleSignOut}>
-      로그아웃
-    </Button>
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="ghost">로그아웃</Button>
+      </DrawerTrigger>
+      <DrawerContent className="mx-auto w-full max-w-screen-sm">
+        <DrawerHeader>
+          <DrawerTitle className="text-center">유저명</DrawerTitle>
+          <DrawerDescription className="text-center">
+            로그아웃하시겠습니까?
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button onClick={handleSignOut}>로그아웃</Button>
+          <DrawerClose asChild>
+            <Button variant="outline">취소</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   )
 }
 
