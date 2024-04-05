2f28905ffc1611de0e2cf1a6acfdaaa6de562bb0
조성원 <tjddnjsjo@kakao.com>
2024-04-05 10:35:31 +0900

    feat: followers, following 탭 추가

--- client/app/(main)/home/page.tsx
+++ client/app/(main)/home/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <>Home</>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+export default async function Home() {
+  return (
+    <Tabs defaultValue="followers">
+      <TabsList className="w-full">
+        <TabsTrigger value="followers" className="w-1/2">
+          Followers
+        </TabsTrigger>
+        <TabsTrigger value="following" className="w-1/2">
+          Following
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="followers">Followers content</TabsContent>
+      <TabsContent value="following">Following content</TabsContent>
+    </Tabs>
+  )
 }
