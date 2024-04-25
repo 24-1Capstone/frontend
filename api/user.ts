6d96855bd0393bdd5fd41e20938f716ea1620955
조성원 <tjddnjsjo@kakao.com>
2024-04-26 05:27:12 +0900

    refactor: profile type 추가

--- api/user.ts
+++ api/user.ts
@@ -1,5 +1,6 @@
 import axiosInstance from './instance'
-import { UserType } from '@/types/user'
+import type { UserType } from '@/types/user'
+import type { ProfileType } from '@/types/profile'
 
 const getUserFollowers = async (): Promise<UserType[]> => {
   const response = await axiosInstance.get('/user/followers')
@@ -11,8 +12,9 @@ const getUserFollowing = async (): Promise<UserType[]> => {
   return response.data
 }
 
-const getUserInfo = async () => {
-  return await axiosInstance.get('/api/users/userinfo')
+const getUserInfo = async (): Promise<ProfileType> => {
+  const response = await axiosInstance.get('/api/user/userinfo')
+  return response.data
 }
 
 export { getUserFollowers, getUserFollowing, getUserInfo }
---
+++ types/profile.ts
@@ -0,0 +1,43 @@
+interface ProfileType {
+  avatarUrl: string
+  bio: string
+  blog: string
+  collaborators: number
+  company: string
+  createdAt: string
+  diskUsage: number
+  email: string
+  eventsUrl: string
+  followers: number
+  followersUrl: string
+  following: number
+  followingUrl: string
+  gistsUrl: string
+  hireable: boolean
+  htmlUrl: string
+  id: number
+  location: string
+  name: string
+  organizationsUrl: string
+  ownedPrivateRepos: number
+  password: string
+  privateGists: number
+  provider: string
+  providerId: string
+  publicGists: number
+  publicRepos: number
+  receivedEventsUrl: string
+  reposUrl: string
+  siteAdmin: boolean
+  starredUrl: string
+  subscriptionsUrl: string
+  totalPrivateRepos: number
+  twitterUsername: string
+  twoFactorAuthentication: boolean
+  type: string
+  updatedAt: string
+  url: string
+  userName: string
+}
+
+export type { ProfileType }
