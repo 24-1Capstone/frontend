b39cc0c95ea5285a58a6d8250e1baee80e365347
조성원 <tjddnjsjo@kakao.com>
2024-05-03 15:31:22 +0900

    feat: 동적 타이틀 & 이전 버튼 조건부 렌더링

--- components/layout/top-app-bar.tsx
+++ components/layout/top-app-bar.tsx
@@ -1,19 +1,35 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import { ChevronLeft } from 'lucide-react'
 
 import { AppBar, Center, End, Start } from '@/components/ui/app-bar'
 import { Button } from '@/components/ui/button'
 import { LogOutButton } from '@/components/log-out-button'
 
+const title: { [key: string]: string } = {
+  home: '홈',
+  calendar: '일정',
+  profile: '프로필',
+  settings: '설정',
+}
+
 function TopAppBar() {
+  const pathname = usePathname()
+
+  const routes = pathname.split('/').filter((path) => path !== '')
+
   return (
     <AppBar>
       <Start>
-        <Button variant="ghost" size="icon">
-          <ChevronLeft className="h-4 w-4" />
-        </Button>
+        {routes.length > 1 && (
+          <Button variant="ghost" size="icon">
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
+        )}
       </Start>
       <Center>
-        <h2 className="text-xl font-bold">Home</h2>
+        <h2 className="text-xl font-bold">{title[routes[0]]}</h2>
       </Center>
       <End>
         <LogOutButton />
