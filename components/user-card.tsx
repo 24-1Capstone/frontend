8989d812038281807ecd455a1dd64bcf46d1a4c3
조성원 <tjddnjsjo@kakao.com>
2024-05-16 10:09:45 +0900

    refactor: user-card 유저명 위치 수정

--- components/user-card.tsx
+++ components/user-card.tsx
@@ -7,13 +7,13 @@ function UserCard({ user }: { user: UserType }) {
   return (
     <Link
       href={`/home/details?user=${user.login}`}
-      className="flex gap-4 rounded-xl p-4 transition hover:bg-foreground/5"
+      className="flex gap-6 rounded-xl p-4 transition hover:bg-foreground/5"
     >
       <Avatar className="h-16 w-16">
         <AvatarImage src={user.avatar_url} draggable={false} />
         <AvatarFallback>{user.login}</AvatarFallback>
       </Avatar>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col justify-center gap-2">
         <span className="font-medium">{user.login}</span>
       </div>
     </Link>
