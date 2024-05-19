84741283aab35d2879ce2bd6aac4f1700f71c385
조성원 <tjddnjsjo@kakao.com>
2024-05-20 03:45:39 +0900

    feat: 커피챗 링크 연결

--- app/(main)/home/details/page.tsx
+++ app/(main)/home/details/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 
 import { Profile } from '@/components/profile'
 import { useUserInfo } from '@/hooks/queries/use-user-info'
+import { buttonVariants } from '@/components/ui/button'
+import { CoffeeIcon } from 'lucide-react'
 
 export default function UserProfile() {
   const searchParams = useSearchParams()
@@ -12,5 +15,18 @@ export default function UserProfile() {
   const { data } = useUserInfo(username ?? '')
   const user = data?.[0]
 
-  return <Profile user={user} />
+  return (
+    <div className="flex flex-col items-center">
+      <Profile user={user} />
+      <Link
+        href={`/meet?user=${username}`}
+        className={buttonVariants({
+          variant: 'outline',
+        })}
+      >
+        <CoffeeIcon className="mr-2 h-4 w-4" />
+        커피챗 신청하기
+      </Link>
+    </div>
+  )
 }
