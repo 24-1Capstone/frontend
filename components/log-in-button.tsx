ce49b7f8fa41c1332b7eca0bc599eca8feb5b439
limbaba1120 <kwlim1120ss@gmail.com>
2024-05-21 17:37:49 +0900

    fix: login button url to /oauth2/authorization/github

--- components/log-in-button.tsx
+++ components/log-in-button.tsx
@@ -7,10 +7,10 @@ function LogInButton() {
   return (
     <Button size="lg" className="w-full" asChild>
       <Link
-        href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/authorize/github`}
+        href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/github`}
       >
         <Github className="mr-2 h-4 w-4" />
-        GitHub으로 계속하기
+        GitHub으로 계속하기....
       </Link>
     </Button>
   )
