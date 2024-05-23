8ef4ce1ef8db447952544cd253f597209ee7859f
조성원 <tjddnjsjo@kakao.com>
2024-05-23 14:11:07 +0900

    refactor: bottom, top 컴포넌트 z-index 수정

--- components/layout/top.tsx
+++ components/layout/top.tsx
@@ -8,7 +8,7 @@ const Top = ({
   children?: React.ReactNode
 }) => {
   return (
-    <div className={cn('sticky top-0 z-50 bg-background', className)}>
+    <div className={cn('sticky top-0 z-40 bg-background', className)}>
       {children}
     </div>
   )
