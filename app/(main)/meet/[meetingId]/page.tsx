ab4455cde1f3032e09d70b40de8c3cbd5b2331c7
조성원 <tjddnjsjo@kakao.com>
2024-05-23 10:52:37 +0900

    feat: 로컬 비디오, 오디오 입출력 기능 구현

---
+++ app/(main)/meet/[meetingId]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { LocalVideo } from '@/components/local-video'
+import { MeetingControls } from '@/components/meeting-controls'
+
+export default function Page() {
+  return (
+    <>
+      <LocalVideo />
+      <MeetingControls />
+    </>
+  )
+}
