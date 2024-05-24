32c641141a7c9cf4760b4c9514b4bfe6595d844d
조성원 <tjddnjsjo@kakao.com>
2024-05-24 15:22:25 +0900

    feat: 커피챗 신청하기 버튼 기능 일시 변경

--- app/(main)/home/details/page.tsx
+++ app/(main)/home/details/page.tsx
@@ -1,33 +1,34 @@
 'use client'
 
 import { Suspense } from 'react'
-import Link from 'next/link'
+import { CoffeeIcon } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
 
 import { Profile } from '@/components/profile'
 import { useUserInfo } from '@/hooks/queries/use-user-info'
-import { buttonVariants } from '@/components/ui/button'
-import { CoffeeIcon } from 'lucide-react'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { createMeeting } from '@/api/meet'
+import { useMyInfo } from '@/hooks/queries/use-my-info'
 
 function UserProfile() {
   const searchParams = useSearchParams()
   const username = searchParams.get('user')
 
-  const { data } = useUserInfo(username ?? '')
-  const user = data?.[0]
+  const { data: userInfo } = useUserInfo(username ?? '')
+  const { data: myInfo } = useMyInfo()
+
+  const handleReserveMeeting = async () => {
+    await createMeeting(myInfo?.[0].login ?? '', userInfo?.[0].login ?? '')
+    alert('커피챗 신청이 완료되었습니다.')
+  }
 
   return (
     <div className="flex flex-col items-center">
-      <Profile user={user} />
-      <Link
-        href={`/meet/device?user=${username}`}
-        className={buttonVariants({
-          variant: 'outline',
-        })}
-      >
+      <Profile user={userInfo?.[0]} />
+      <Button variant="outline" onClick={handleReserveMeeting}>
         <CoffeeIcon className="mr-2 h-4 w-4" />
         커피챗 신청하기
-      </Link>
+      </Button>
     </div>
   )
 }
