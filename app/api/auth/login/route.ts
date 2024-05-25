d969a10fe49576062ebbab67cc7953e473d4aab6
조성원 <tjddnjsjo@kakao.com>
2024-05-25 22:50:16 +0900

    fix: route handler no response 오류 수정

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -5,18 +5,19 @@ import { getAccessToken } from '@/api/auth'
 export async function GET(request: NextRequest) {
   const refreshToken = request.cookies.get('refresh_token')
 
+  const url = request.nextUrl.clone()
+  url.pathname = '/'
+
+  const response = NextResponse.redirect(url)
+
   if (refreshToken) {
     const { data } = await getAccessToken(refreshToken.value)
 
-    const url = request.nextUrl.clone()
-    url.pathname = '/'
-
-    const response = NextResponse.redirect(url)
     response.cookies.set('token', data.accessToken, {
       maxAge: 3600,
       path: '/',
     })
-
-    return response
   }
+
+  return response
 }
