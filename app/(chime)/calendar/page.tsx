a2d89f697f8b5d81bdb616ed05ce3fcd6393413b
조성원 <tjddnjsjo@kakao.com>
2024-05-26 14:06:43 +0900

    feat: 일정 페이지 리디자인

--- app/(chime)/calendar/page.tsx
+++ app/(chime)/calendar/page.tsx
@@ -2,15 +2,39 @@
 
 import { useAllMeetings } from '@/hooks/queries/use-all-meetings'
 import { MeetingCard } from '@/components/meeting-card'
+import { useMyInfo } from '@/hooks/queries/use-my-info'
 
 export default function Page() {
   const { data: meetings } = useAllMeetings()
+  const { data: myInfo } = useMyInfo()
+
+  const appliedMeetings = meetings?.filter(
+    (meeting) => meeting.applyUserName === myInfo?.[0].login,
+  )
+  const receivedMeetings = meetings?.filter(
+    (meeting) => meeting.receiveUserName === myInfo?.[0].login,
+  )
 
   return (
     <div className="flex flex-col gap-4">
-      {meetings?.map((data) => (
-        <MeetingCard key={data.externalMeetingId} data={data} />
-      ))}
+      <h3 className="text-2xl font-bold">내가 신청한 커피챗</h3>
+      {appliedMeetings?.length === 0 ? (
+        <div className="py-4 text-foreground/30">신청한 커피챗이 없습니다.</div>
+      ) : (
+        appliedMeetings?.map((data) => (
+          <MeetingCard key={data.externalMeetingId} data={data} />
+        ))
+      )}
+      <h3 className="mt-8 text-2xl font-bold">내가 신청받은 커피챗</h3>
+      {receivedMeetings?.length === 0 ? (
+        <div className="py-4 text-foreground/30">
+          신청받은 커피챗이 없습니다.
+        </div>
+      ) : (
+        receivedMeetings?.map((data) => (
+          <MeetingCard key={data.externalMeetingId} data={data} />
+        ))
+      )}
     </div>
   )
 }
