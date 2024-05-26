eb83131a691086f9f52399ba06417bcce8f4a13d
조성원 <tjddnjsjo@kakao.com>
2024-05-26 14:28:33 +0900

    fix: vercel 로그 문제 수정

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -19,7 +19,7 @@ export async function GET(request: NextRequest) {
         path: '/',
       })
     } catch (error) {
-      throw new Error('Invalid refresh token')
+      console.error('Invalid refresh token', error)
     }
   }
 
