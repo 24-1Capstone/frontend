91d01131ebaaf4bd96ac864f6e20f1da38456c5e
조성원 <tjddnjsjo@kakao.com>
2024-05-31 11:15:27 +0900

    feat: 설정 페이지 계정 관련 버튼 추가

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -1,3 +1,12 @@
+import { DeleteAccountButton } from '@/components/delete-account-button'
+import { LogOutButton } from '@/components/logout-button'
+
 export default function Page() {
-  return <>Settings</>
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="text-2xl font-bold">계정</h3>
+      <LogOutButton />
+      <DeleteAccountButton />
+    </div>
+  )
 }
---
+++ components/logout-button.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import Cookies from 'js-cookie'
+
+import { logOut } from '@/api/auth'
+import { useMyInfo } from '@/hooks/queries/use-my-info'
+import { Button } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from './ui/drawer'
+
+function LogOutButton() {
+  const router = useRouter()
+  const { data: user } = useMyInfo()
+
+  const handleLogOut = () => {
+    logOut().then(() => {
+      Cookies.remove('token')
+      Cookies.remove('refresh_token')
+      router.push('/')
+    })
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button>로그아웃</Button>
+      </DrawerTrigger>
+      <DrawerContent className="mx-auto w-full max-w-screen-sm">
+        <DrawerHeader>
+          <DrawerTitle className="text-center">{user?.[0].name}</DrawerTitle>
+          <DrawerDescription className="text-center">
+            로그아웃하시겠습니까?
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button onClick={handleLogOut}>로그아웃</Button>
+          <DrawerClose asChild>
+            <Button variant="outline">취소</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export { LogOutButton }
