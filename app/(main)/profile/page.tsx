47bb7576aa1df1b21455a0999c9b70f7a2f2f929
조성원 <tjddnjsjo@kakao.com>
2024-05-31 09:28:26 +0900

    refactor: change page.tsx into server component

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,18 +1,27 @@
 'use client'
 
+import { Suspense } from 'react'
+
 import { Profile } from '@/components/profile'
 import { useMyInfo } from '@/hooks/queries/use-my-info'
 
-export default function Page() {
-  const { data } = useMyInfo()
-  const user = data?.[0]
+function MyProfile() {
+  const { data: user } = useMyInfo()
 
   return (
     <>
-      <Profile user={user} />
+      <Profile user={user?.[0]} />
       <p className="pt-16 text-center text-sm">
         프로필 수정은 GitHub를 통해서 할 수 있습니다.
       </p>
     </>
+  )
+}
+
+export default function Page() {
+  return (
+    <Suspense>
+      <MyProfile />
+    </Suspense>
   )
 }
