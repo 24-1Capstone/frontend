164c3da74dc45fad0c70a2a87b2792c185d6ec09
조성원 <tjddnjsjo@kakao.com>
2024-06-02 05:31:53 +0900

    feat: 나의 커피챗 페이지 패딩 추가

--- app/(chime)/my-chat/page.tsx
+++ app/(chime)/my-chat/page.tsx
@@ -3,7 +3,7 @@ import { ReceivedMeetingsList } from '@/components/received-meetings-list'
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 py-4">
       <h3 className="text-2xl font-bold">내가 신청한 커피챗</h3>
       <AppliedMeetingsList />
       <h3 className="mt-8 text-2xl font-bold">내가 신청받은 커피챗</h3>
