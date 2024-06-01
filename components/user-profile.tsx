8ac54c0551027aa888b454fa78ad9ae6e5c6a4dc
조성원 <tjddnjsjo@kakao.com>
2024-06-02 05:27:16 +0900

    refactor: 커피챗 신청하기 버튼 기능 및 disabled 로직 수정

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { CoffeeIcon } from 'lucide-react'
 
 import { useMyInfo } from '@/hooks/queries/use-my-info'
@@ -10,6 +10,7 @@ import { Profile } from '@/components/profile'
 import { createMeeting } from '@/api/meet'
 
 function UserProfile() {
+  const router = useRouter()
   const searchParams = useSearchParams()
   const username = searchParams.get('user')
 
@@ -17,16 +18,19 @@ function UserProfile() {
   const { data: userInfo, isLoading } = useUserInfo(username ?? '')
 
   const handleReserveMeeting = async () => {
-    if (!isLoading) {
-      await createMeeting(myInfo?.[0].login ?? '', userInfo?.[0].login ?? '')
-      alert('커피챗 신청이 완료되었습니다.')
-    }
+    router.push(`/reserve?user=${userInfo?.[0].login}`)
+    // await createMeeting(myInfo?.[0].login ?? '', userInfo?.[0].login ?? '')
+    // alert('커피챗 신청이 완료되었습니다.')
   }
 
   return (
     <div className="flex flex-col items-center">
       <Profile user={userInfo?.[0]} />
-      <Button variant="outline" onClick={handleReserveMeeting}>
+      <Button
+        variant="outline"
+        onClick={handleReserveMeeting}
+        disabled={isLoading}
+      >
         <CoffeeIcon className="mr-2 h-4 w-4" />
         커피챗 신청하기
       </Button>
