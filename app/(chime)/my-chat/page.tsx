49025ab6ee017d835e7488b11eddb4f66d36c684
조성원 <tjddnjsjo@kakao.com>
2024-06-02 21:35:16 +0900

    feat: meeting에서 reservation으로 표시할 데이터 변경

--- app/(chime)/my-chat/page.tsx
+++ app/(chime)/my-chat/page.tsx
@@ -1,13 +1,13 @@
-import { AppliedMeetingsList } from '@/components/applied-meetings-list'
-import { ReceivedMeetingsList } from '@/components/received-meetings-list'
+import { AppliedReservationsList } from '@/components/applied-reservations-list'
+import { ReceivedReservationsList } from '@/components/received-reservations-list'
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-4 py-4">
       <h3 className="text-2xl font-bold">내가 신청한 커피챗</h3>
-      <AppliedMeetingsList />
+      <AppliedReservationsList />
       <h3 className="mt-8 text-2xl font-bold">내가 신청받은 커피챗</h3>
-      <ReceivedMeetingsList />
+      <ReceivedReservationsList />
     </div>
   )
 }
