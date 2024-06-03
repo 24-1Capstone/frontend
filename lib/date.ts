68fa4838d39414124714e7a6d60cda9e375d717f
조성원 <tjddnjsjo@kakao.com>
2024-06-03 21:44:15 +0900

    refactor: date util 함수 한번에 named export하도록 수정

--- lib/date.ts
+++ lib/date.ts
@@ -1,17 +1,19 @@
 import { addHours, format, set } from 'date-fns'
 import { ko } from 'date-fns/locale'
 
-export function combineDateTime(date: Date, time: string): Date {
+function combineDateTime(date: Date, time: string): Date {
   const hours = parseInt(time.split(':')[0], 10) + 9
   const minutes = parseInt(time.split(':')[1], 10)
 
   return set(date, { hours, minutes })
 }
 
-export function calcEndTime(date: Date): Date {
+function calcEndTime(date: Date): Date {
   return addHours(date, 1)
 }
 
-export function formatDateTime(date: string): string {
+function formatDateTime(date: string): string {
   return format(date, 'M월 d일 HH시', { locale: ko })
 }
+
+export { combineDateTime, calcEndTime, formatDateTime }
