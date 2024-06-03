78367da69f5445893fd1366308ddd6c31659eda5
조성원 <tjddnjsjo@kakao.com>
2024-06-03 23:43:46 +0900

    refactor: 예약 페이지 컴포넌트 분리

---
+++ components/profile-min.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+import { useUserInfo } from '@/hooks/queries/use-user-info'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+function ProfileMin() {
+  const searchParams = useSearchParams()
+  const username = searchParams.get('user')
+
+  const { data: userInfo, isLoading } = useUserInfo(username ?? '')
+
+  return (
+    <div className="my-8 flex items-center gap-4">
+      <Avatar className="h-16 w-16">
+        <AvatarImage src={userInfo?.[0].avatar_url} draggable={false} />
+        <AvatarFallback>{userInfo?.[0].login}</AvatarFallback>
+      </Avatar>
+      <p>
+        <span className="font-semibold">
+          {userInfo?.[0].name}({userInfo?.[0].login})
+        </span>
+        님께
+        <br />
+        커피챗을 신청합니다.
+      </p>
+    </div>
+  )
+}
+
+export { ProfileMin }
