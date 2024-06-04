e925619b89ca22fc1f44fb36a107b9edafffa784
조성원 <tjddnjsjo@kakao.com>
2024-06-05 04:51:23 +0900

    feat: 액세스 토큰 만료 시 리프레시 토큰으로 액세스 토큰 재발급

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,32 @@ import type { NextRequest, NextFetchEvent } from 'next/server'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
-  const session = req.cookies.get('token')
   const { pathname } = req.nextUrl
+  const session = req.cookies.has('token')
+  const refreshToken = req.cookies.get('refresh_token')
+
+  if (refreshToken && !session && pathname !== '/api/auth/login') {
+    return await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refreshToken: refreshToken.value }),
+    }).then(async (res) => {
+      const data = await res.json()
+
+      const response =
+        pathname === '/'
+          ? NextResponse.redirect(new URL('/users', req.url))
+          : NextResponse.next()
+      response.cookies.set('token', data.accessToken, {
+        maxAge: 3600,
+        path: '/',
+      })
+
+      return response
+    })
+  }
 
   if (session) {
     if (pathname === '/')
