2a3408fbf5042dce96214f3ca25b7ae8d9bf79be
조성원 <tjddnjsjo@kakao.com>
2024-06-04 20:00:25 +0900

    fix: favicon 경로 오류 수정 및 manifest 변경

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#fff',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
       },
       {
         src: '/android-chrome-192x192.png',
